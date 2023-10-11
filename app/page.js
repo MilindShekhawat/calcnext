3f414bf92f13bce472a3e96b96b015f400e2e215
MilindShekhawat <milindshekhawat10@gmail.com>
2023-10-11 12:36:54 +0530

    Completed

--- app/page.js
+++ app/page.js
@@ -1,94 +1,57 @@
-import Image from 'next/image'
+"use client"
 import styles from './page.module.css'
+import { useState } from 'react';
 
 export default function Home() {
+  const [expression, setExpression] = useState("");
+  
+  const buttons = [
+    ["(", "(", styles.operatorButton],[")", ")", styles.operatorButton],["del", "del", styles.specialButton],["/", "÷", styles.operatorButton],
+    ["7", "7",                      ],["8", "8",                      ],["9", "9",                         ],["*", "×", styles.operatorButton],
+    ["4", "4",                      ],["5", "5",                      ],["6", "6",                         ],["-", "−", styles.operatorButton],
+    ["1", "1",                      ],["2", "2",                      ],["3", "3",                         ],["+", "+", styles.operatorButton],
+    [".", ".",                      ],["0", "0",                      ],["C", "C", styles.specialButton    ],["=", "=", styles.specialButton ]
+  ];
+  
+
+  function handleButtonClick(buttonPressed) {
+    //alert(typeof expression);
+    switch(buttonPressed) {
+      case "=":
+        try {
+          const result = eval(expression).toString();   
+          setExpression(result);
+        } catch (error) {
+          setExpression("Invalid Input");
+        }
+        break;
+
+      case "del":
+        let expressionString = expression;
+        expressionString = expressionString.slice(0, -1);
+        setExpression(expressionString);
+        break;
+
+      case "C":
+        setExpression("0");
+        break;
+      
+      default:
+        setExpression((preExp) => preExp + buttonPressed);
+    }
+  }
+    
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className={styles.calcContainer}>
+        <input type="text" className={styles.textBox} value={expression} readOnly/>
+        <div className={styles.buttonContainer}>
+          {buttons.map((button) => (
+            <button key={button[0]} className={button[2]} onClick={() => handleButtonClick(button[0])}>
+              {button[1]}
+            </button>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
